3a8656bee1aa773906d68965eaecd7d192199ca1
Ivaylo Ignatov <ivaylo.ignatov@car-digital.eu>
2026-03-11 14:26:02 +0200

    adding no scroll when mobile menu is open

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, Renderer2 } from '@angular/core';
 import { Router } from '@angular/router';
 import { Paths } from '../../enums/paths.enum';
 import { AuthService } from '../../services/auth.service';
@@ -11,21 +11,39 @@ import { AuthService } from '../../services/auth.service';
 })
 export class NavbarComponent {
   menuOpen: boolean = false;
-  isDesktop: boolean = false;
   paths = Paths;
 
   constructor(
     private readonly router: Router,
-    public readonly authService: AuthService
+    public readonly authService: AuthService,
+    private readonly renderer: Renderer2
   ) { }
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
+
+    if (this.menuOpen) {
+      this.renderer.addClass(document.body, 'no-scroll');
+    } else {
+      this.renderer.removeClass(document.body, 'no-scroll');
+    }
   }
 
   goTo(path: string, event: Event) {
     event.preventDefault();
     this.router.navigate([path]);
-    this.menuOpen = false;
+    this.closeMenuAndRemoveNoScroll();
+  }
+
+  private closeMenuAndRemoveNoScroll() {
+    if (this.menuOpen) {
+      this.menuOpen = false;
+      this.renderer.removeClass(document.body, 'no-scroll');
+    }
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.closeMenuAndRemoveNoScroll();
   }
 }
